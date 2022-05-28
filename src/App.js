0052b207d9bfe86cf9223020bbd597ee48ac1325
Bethpen <64626802+Bethpen@users.noreply.github.com>
2022-05-28 17:53:11 +0100

    update App.js

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
           <Route path="/">
             <Route index element={<Login />} />
             <Route path="home" element={<Home />} />
-            <Route path="login" element={<Login />} />
+            <Route path="logeenn" element={<Login />} />
             <Route path="users">
               <Route index element={<List />} />
               <Route path=":userId" element={<Single />} />
